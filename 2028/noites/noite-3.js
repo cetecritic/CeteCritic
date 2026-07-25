9c1f8c94a0a9cad12351bc4316bd3bcb7e923c63
Cetecritic <cetecritic@gmail.com>
2026-07-25 18:10:00 -0300

    V26.8.9

--- 2028/noites/noite-3.js
+++
@@ -1,8 +0,0 @@
-/* ===== NOITE 3 — CETEC FESTIVAL 2027 ===== */
-
-NOITES[3] = {
-  data: '2028-07-12T19:00:00-03:00',
-  subtitulo: 'Turmas que se apresentaram na terceira noite do festival.',
-  pecas: [
-  ]
-};
